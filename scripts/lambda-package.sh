1f1e295c518202738776c118019efce6fcb3576e
DilanAriza <dilansantiagoariza@gmail.com>
2023-07-07 10:46:54 -0500

    Feature integrate nodemodules with code

--- scripts/lambda-package.sh
+++ scripts/lambda-package.sh
@@ -4,18 +4,18 @@ randomCodeVersion=$(echo $RANDOM | md5sum | head -c 20; echo)
 FUNCTION_NAME=$(aws ssm get-parameter --name "/lambda/MoviesFunctionArn" --query "Parameter.Value" --output text | awk -F'[:/]' '{print $NF}')
 
 # Package node_modules as a layer
-echo Creating layer of dependencies
-zip -r -9 node_modules.zip node_modules
+# echo Creating layer of dependencies
+# zip -r -9 node_modules.zip node_modules
 
-# Upload layer to S3
-echo Upload layer to s3
-aws s3 cp node_modules.zip s3://movies-lambdas-code/layers/node_modules-$randomCodeVersion.zip
+# # Upload layer to S3
+# echo Upload layer to s3
+# aws s3 cp node_modules.zip s3://movies-lambdas-code/layers/node_modules-$randomCodeVersion.zip
 
-# Creating layer
-LAYER_ARN=$(aws lambda publish-layer-version --layer-name movies_node_modules --content S3Bucket=movies-lambdas-code,S3Key=layers/node_modules-$randomCodeVersion.zip --compatible-runtimes nodejs18.x | jq -r '.LayerVersionArn')
-aws lambda update-function-configuration --function-name $FUNCTION_NAME --layers $LAYER_ARN
+# # Creating layer
+# LAYER_ARN=$(aws lambda publish-layer-version --layer-name movies_node_modules --content S3Bucket=movies-lambdas-code,S3Key=layers/node_modules-$randomCodeVersion.zip --compatible-runtimes nodejs18.x | jq -r '.LayerVersionArn')
+# aws lambda update-function-configuration --function-name $FUNCTION_NAME --layers $LAYER_ARN
 
-rm -rf node_modules
+# rm -rf node_modules
 
 # Package of code
 echo Creating package of code
