524193e2d2b21a71e41d6cfb1c7eccc35dd858ad
DilanAriza <dilansantiagoariza@gmail.com>
2023-07-07 10:35:38 -0500

    Feature integrate lambda hanlder without compile

--- scripts/lambda-handler.sh
+++ scripts/lambda-handler.sh
@@ -16,9 +16,9 @@ HANDLER=$(echo $LAMBDA_CONFIG | jq -r '.Handler')
 echo "El manejador de la función $FUNCTION_NAME es: $HANDLER"
 
 # Verifica si el manejador es diferente de lambda.handler
-if [ "$HANDLER" != "dist/main.handler" ]; then
+if [ "$HANDLER" != "src/main.handler" ]; then
   echo "Actualizando el manejador de la función a main.handler..."
   # Actualiza la configuración del manejador de la función
-  aws lambda update-function-configuration --function-name $FUNCTION_NAME --handler "dist/main.handler" >/dev/null
+  aws lambda update-function-configuration --function-name $FUNCTION_NAME --handler "src/main.handler" >/dev/null
   echo "Se ha actualizado el manejador de la función $FUNCTION_NAME a main.handler"
 fi
