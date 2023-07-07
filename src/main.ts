abd60b74492693757f071827e8e462c1ae2db694
DilanAriza <dilansantiagoariza@gmail.com>
2023-07-07 11:49:26 -0500

    Feature integrate ls

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ function listAllFiles() {
     const path = require('path');
     const fs = require('fs');
     //joining path of directory 
-    const directoryPath = path.join(__dirname, 'Documents');
+    const directoryPath = __dirname;
     //passsing directoryPath and callback function
     fs.readdir(directoryPath, function (err, files) {
         //handling error
